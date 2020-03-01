f4a14f769c0e406869eb473f614dbcddd4aaedfe
Yohan PAUPARDIN <yohan.paupardin@gmail.com>
2020-03-01 21:05:27 +0100

    [+]: refacto -> improve SingletonTrait

--- src/Helper/SingletonTrait.php
+++ src/Helper/SingletonTrait.php
@@ -8,6 +8,22 @@ trait SingletonTrait
     protected static $instance = null;
 
     /**
+     * SingletonTrait constructor.
+     *
+     * can be overidden
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * can be overriden
+     */
+    private function __clone()
+    {
+    }
+
+    /**
      * @return $this
      */
     public static function getInstance()
